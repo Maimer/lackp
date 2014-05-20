b1cbc5a626a7968fd5d9b22760a5636427dc98bf
Nicholas Lee <nesnl23@yahoo.com>
2014-05-20 13:53:34 -0400

    Added Team Page

--- server.rb
+++ server.rb
@@ -2,51 +2,76 @@ require 'sinatra'
 require 'uri'
 require 'csv'
 
-get '/' do
-
-  lackp_roster = {}
+def make_data()
+  @lackp_roster = {}
 
   CSV.foreach('lackp_starting_rosters.csv', headers: true) do |row|
-    if !lackp_roster.has_key?(row["team"])
-      lackp_roster[row["team"]] = []
+    if !@lackp_roster.has_key?(row["team"])
+      @lackp_roster[row["team"]] = []
     end
-    lackp_roster[row["team"]] << [row["first_name"], row["last_name"], row["position"]]
+    @lackp_roster[row["team"]] << [row["first_name"], row["last_name"], row["position"]]
   end
+  @lackp_roster
+end
 
+def make_teams(roster)
   @teams = []
-  @positions = []
-
-  lackp_roster.each do |team, player|
+  roster.each do |team, player|
     @teams << team
+  end
+  @teams
+end
+
+def make_positions(roster)
+  @positions = []
+  roster.each do |team, player|
     player.each do |data|
       if !@positions.include?(data[2])
         @positions << data[2]
       end
     end
   end
+  @positions
+end
+
+def make_roster(data, teamname)
+  @rosterhash = {}
+  data[teamname].each do |player|
+    @rosterhash[player[0] + " " + player[1]] = player[2]
+  end
+  @rosterhash
+end
+
+get '/' do
+  @data = make_data()
+  @teams = make_teams(@data)
+  @positions = make_positions(@data)
+
   erb :index
 end
 
 get '/team/:team_name' do
-  @teams = params[:team_name]
+  @team_name = params[:team_name]
+  @data = make_data()
+  @roster = make_roster(@data, @team_name)
   # The :team_name is available in our params hash
   erb :teampage
 end
 
 get '/team/URI.encode(:team_name)' do
-  @teams = params[:team_name]
+  @team_name = params[:team_name]
   # The :team_name is available in our params hash
   erb :teampage
 end
 
 get '/position/:position_name' do
-  @teams = params[:position_name]
+  @position_name = params[:position_name]
   # The :position_name is available in our params hash
   erb :positionpage
 end
 
 get '/position/URI.encode(:position_name)' do
-  @teams = params[:position_name]
+  @position_name = params[:position_name]
   # The :position_name is available in our params hash
   erb :positionpage
 end
